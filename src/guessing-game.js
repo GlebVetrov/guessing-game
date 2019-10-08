d5e99ce998e9e985796a9fe12395480e9f034b42
Gleb <3696732@mail.ru>
2019-10-08 23:12:11 +0300

    feat: add task with solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,18 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.mid = Math.ceil((min + max) / 2);
     }
 
     guess() {
-
+        return this.mid;
     }
 
     lower() {
-
+        this.max = this.mid;
+        this.mid = Math.ceil((this.min + this.max) / 2);
     }
 
     greater() {
+        this.min = this.mid;
+        this.mid = Math.ceil((this.min + this.max) / 2);
 
     }
 }
